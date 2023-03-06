22bd37087300a75dfd31256df13b530ca7bb06f8
K1kva2006 <levanikikva2006@gmail.com>
2023-03-06 18:02:22 +0400

    add download & JS

---
+++ app.js
@@ -0,0 +1,15 @@
+const download = document.querySelectorAll(".card-download");
+const downloadButton = document.querySelectorAll(".download");
+
+download.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.style.color = "green";
+
+  });
+});
+
+downloadButton.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.textContent = "გადმოწერილია";
+  });
+});
